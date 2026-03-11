b26c61c14893540233666c56462b948933c13af6
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-11 15:39:19 +0530

    Search in Rotated Sorted Array

---
+++ Arrays/33_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,15 @@
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Medium
+// Leetcode - https://leetcode.com/problems/search-in-rotated-sorted-array
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i = 0;i < nums.size();i++){
+            if(target == nums[i]) return i;
+        }
+        return -1;
+    }
+};
