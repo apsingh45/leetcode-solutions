afdf7d1b97bae684c1ca62981d03152650d36d4f
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-11 12:14:58 +0530

    Palindrome Number

---
+++ Arrays/9_Palindrome_Number.cpp
@@ -0,0 +1,29 @@
+// Time Complexity - O(log10(n))
+// Space Complexity - O(1)
+
+// Medium
+// LEetcode - https://leetcode.com/problems/palindrome-number
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return 0;
+        int temp=x;
+        long rev=0;
+         while(x!=0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x/=10;
+
+            if(rev>INT_MAX||rev<INT_MIN){
+                return 0;
+            }
+        }
+        if(rev==temp){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
