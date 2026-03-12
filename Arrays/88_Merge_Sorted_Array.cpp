58e5c3be52e5b2cd3f9cae635aeab6eb0f310549
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-12 10:28:35 +0530

    Merge Sorted Array

---
+++ Arrays/88_Merge_Sorted_Array.cpp
@@ -0,0 +1,15 @@
+//Time Complexity - O((m+n) log(m+n))
+// Space Complexity - O(1)
+
+// Easy
+// LeetCode - https://leetcode.com/problems/merge-sorted-array
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = 0; i < n; i++){
+            nums1[m+i] = nums2[i];
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
