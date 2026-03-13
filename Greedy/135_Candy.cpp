18c4e3d3f508b54d162737e543db1332070be0b3
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-13 12:51:57 +0530

    Candy

---
+++ Greedy/135_Candy.cpp
@@ -0,0 +1,28 @@
+// Time Complexity - O(n)
+// Space Complexity -O(n)
+
+// Hard
+// LeetCode - https://leetcode.com/problems/candy
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n =  ratings.size();
+        vector<int> v(n, 1);
+         for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                v[i] = v[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                v[i] = max(v[i], v[i + 1] + 1);
+            }
+        }
+        int candy = 0;
+        for(int x : v) {
+            candy += x;
+        }
+        return candy;
+    }
+};
