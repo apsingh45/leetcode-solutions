05989955a171c4a37561df6962b8525bbd2cf3f8
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-15 19:20:55 +0530

    Evaluate Reverse Polish Notation

---
+++ Stack/150_Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,27 @@
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+
+// Medium 
+// Leetcode - https://leetcode.com/problems/evaluate-reverse-polish-notation
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st ;
+        for(string& token : tokens){
+            if(token == "+" || token == "-" || token =="*" || token == "/"){
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+
+                if(token == "+") st.push(a+b);
+                else if(token == "-") st.push(a-b);
+                else if(token == "*") st.push(a*b);
+                else if(token == "/") st.push(a/b); 
+            }
+            else st.push(stoi(token));
+        }
+        return st.top();
+    }
+};
