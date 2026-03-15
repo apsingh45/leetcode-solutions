6fa35a9c3944a87125b2960db8025940d9c02575
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-15 20:17:25 +0530

    Rotate Array

---
+++ Arrays/189_Rotate_Array.cpp
@@ -0,0 +1,15 @@
+//Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Medium
+// Leetcode - https://leetcode.com/problems/rotate-array
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
