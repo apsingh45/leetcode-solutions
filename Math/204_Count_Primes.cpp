95c9b09c7e85296303e6fed0c20790a320b83fd5
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-15 21:13:16 +0530

    Count Primes

---
+++ Math/204_Count_Primes.cpp
@@ -0,0 +1,27 @@
+//Time Complexity - O(n log n)
+// Space Complexity - O(n)
+
+// Medium
+// Leetcode - https://leetcode.com/problems/count-primes
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2) return 0;
+        vector<bool> isPrime(n,true);
+        isPrime[0] = isPrime[1] = false;
+
+        for(int i = 2; i*i <= n ;i++){
+            if(isPrime[i]){
+                for(int j = i*i; j < n; j += i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 2; i <n ;i++){
+            if(isPrime[i]) count++;
+        }
+        return count;
+    }
+};
