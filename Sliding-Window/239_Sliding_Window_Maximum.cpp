fe4941a4f501a0e6500252d266021b76954a6282
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-16 08:17:11 +0530

    Sliding Window Maximum

---
+++ Sliding-Window/239_Sliding_Window_Maximum.cpp
@@ -0,0 +1,36 @@
+// Time Complexity - O(n)
+// Space Complexity - O(k)
+
+// Hard
+// Leetcode - https://leetcode.com/problems/sliding-window-maximum
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>dq;
+        vector<int>result;
+
+        for(int i = 0;i<k;i++){
+            while(!dq.empty() && nums[i] >= nums[dq.back()]){
+                dq.pop_back();  
+            }
+            dq.push_back(i);
+        }
+
+        result.push_back(nums[dq.front()]);
+
+        for(int i = k;i<nums.size();i++){
+            if(dq.front() == i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[i] >= nums[dq.back()]){
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+            result.push_back(nums[dq.front()]);
+        }
+
+        return result;
+    }
+};
