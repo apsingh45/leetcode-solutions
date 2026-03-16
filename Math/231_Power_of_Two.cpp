d1d2dae2161e160e7ed15c9b585a9c0fbb1bf5b9
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-16 07:58:34 +0530

    Power of Two

---
+++ Math/231_Power_of_Two.cpp
@@ -0,0 +1,16 @@
+// Time Complexity - O(log n)
+// Space Complexity - O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/power-of-two
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return 0;
+        while(n%2==0){
+            n=n/2;
+        }
+        return n==1;
+    }
+};
