5fd969b114171c00d196ca91dbb6aebbee2754b7
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-17 08:49:36 +0530

    Intersection of Two Arrays

---
+++ Two-Pointers/349_Intersection_of_Two_Arrays.cpp
@@ -0,0 +1,35 @@
+// Time Complexity - O(nlogn + mlogm)
+// Space Complexity - O(k)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/intersection-of-two-arrays
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0,j=0;
+        set<int>result;
+        while(i<nums1.size()&&j<nums2.size()){
+            if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else if(nums2[j]>nums1[i]){
+                i++;
+            }
+            else{
+                result.insert(nums1[i]);
+                i++;
+                j++;
+            }
+
+        }
+        vector<int>output;
+        for(auto i:result){
+            output.push_back(i);
+        }
+        return output;
+
+    }
+};
