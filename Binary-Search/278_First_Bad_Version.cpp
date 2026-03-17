714806b0765ef6495d4f2e84bf8a880a141c2b95
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-17 08:25:18 +0530

    First Bad Version

---
+++ Binary-Search/278_First_Bad_Version.cpp
@@ -0,0 +1,24 @@
+// Time Complexity - O(log n)
+// Space Complexity - O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/first-bad-version
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int start = 1;
+        int end = n ;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(isBadVersion(mid)) end = mid - 1;
+            else if(!isBadVersion(mid)) {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+};
