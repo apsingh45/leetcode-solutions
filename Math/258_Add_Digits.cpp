b6d71819c6b6474a42abd2024e925b7b86ab1f8a
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-17 08:11:50 +0530

    Binary Tree Paths

---
+++ Math/258_Add_Digits.cpp
@@ -0,0 +1,18 @@
+// Time Complexity - O(log n)
+// Space Complexity - O(log n)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/add-digits
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num < 10) return num;
+        int temp = 0;
+        while(num > 0){
+            temp = temp + num % 10; 
+            num /= 10; 
+        }
+        return addDigits(temp);
+    }
+};
