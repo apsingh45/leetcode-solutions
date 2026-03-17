b783420de5b66d4cad75b19b977f9e4318f64869
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-18 01:04:06 +0530

    Find the Difference

---
+++ Strings/389_Find_the_Difference.cpp
@@ -0,0 +1,17 @@
+// Time Complexity - O(n log n)
+// Space Complexity - O(n)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/find-the-difference
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i = 0 ;i < s.size();i++){
+            if(s[i] != t[i]) return t[i];
+        }
+        return t[t.size()-1];
+    }
+};
