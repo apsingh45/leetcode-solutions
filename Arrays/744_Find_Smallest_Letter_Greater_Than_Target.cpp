821fb96ec5e1b460fd9ad73a268abcf784998d36
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-21 19:56:21 +0530

    find Smallest Letter Greater Than Target

---
+++ Arrays/744_Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,17 @@
+// Time Complexity - O(n)
+// Space Complexity -O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/find-smallest-letter-greater-than-target
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(int i =0;i<letters.size();i++){
+            if(letters[i]>target){
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+};
