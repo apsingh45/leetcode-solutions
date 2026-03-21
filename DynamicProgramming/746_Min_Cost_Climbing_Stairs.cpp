e3c03719e6561e568893d12564e45f6c56891bec
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-21 19:59:50 +0530

    Min Cost Climing Stairs

---
+++ DynamicProgramming/746_Min_Cost_Climbing_Stairs.cpp
@@ -0,0 +1,20 @@
+// Time Complexity - O(n)
+// Space Complexity -O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/min-cost-climbing-stairs
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+        for (int i = 2; i < n; i++) {
+            int curr = cost[i] + min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return min(prev1, prev2);
+    }
+};
