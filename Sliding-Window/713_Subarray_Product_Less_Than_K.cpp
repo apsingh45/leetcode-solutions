b1b3c38d17a7bbdb20457447b0039632f58a2f3b
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-21 19:38:48 +0530

    Subarray Product Less Than k

---
+++ Sliding-Window/713_Subarray_Product_Less_Than_K.cpp
@@ -0,0 +1,21 @@
+// Time Complexity - O(n)
+// Space Complexity -O(1)
+
+// Medium
+// Leetcode - https://leetcode.com/problems/subarray-product-less-than-k
+
+class Solution {
+public:
+  int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+    int left = 0, right = 0, product = 1, count = 0;
+    int n = nums.size();
+    if(k <= 1) return 0;
+    while (right < n) {
+      product *= nums[right];
+      while (product >= k) product /= nums[left++];
+      count += 1 + (right - left);
+      right++;
+    }
+    return count;
+  }
+};
