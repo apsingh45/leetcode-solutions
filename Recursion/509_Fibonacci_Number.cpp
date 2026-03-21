4534183c51db436f30238b7d65aab6be0ed7f6d9
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-21 17:27:32 +0530

    Fibonacci Number

---
+++ Recursion/509_Fibonacci_Number.cpp
@@ -0,0 +1,17 @@
+// Time Complexity - O(2^n)
+// Space Complexity -O(n)
+
+// Easy 
+// Leetcode - https://leetcode.com/problems/fibonacci-number
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+        return -1;
+    }
+};
