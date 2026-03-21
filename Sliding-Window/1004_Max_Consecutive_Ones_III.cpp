e4894942553955f53c2082652ee4cdb2c7e065cd
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-21 22:31:29 +0530

    Max Consecutive Ones III

---
+++ Sliding-Window/1004_Max_Consecutive_Ones_III.cpp
@@ -0,0 +1,25 @@
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Medium
+// Leetcode - https://leetcode.com/problems/max-consecutive-ones-iii
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i=0,j=0;
+        while(j<nums.size()){
+            if(nums[j]==0){
+                k--;
+            }
+            if(k<0){
+                if(nums[i]==0){
+                    k++;
+                }
+                i++;
+            }
+            j++;
+        }
+        return j-i;
+    }
+};
