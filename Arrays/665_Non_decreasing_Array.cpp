84bd10351ac45b3a9a87f7b40c3c6a80b75b9bab
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-21 18:37:08 +0530

    Non-decreasing Array

---
+++ Arrays/665_Non_decreasing_Array.cpp
@@ -0,0 +1,25 @@
+// Time Complexity - O(n)
+// Space Complexity -O(1)
+
+// Medium
+// Leetcode - https://leetcode.com/problems/non-decreasing-array
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int count = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+                if (count > 1) return false;
+                if (i > 0 && nums[i - 1] > nums[i + 1]) {
+                        nums[i + 1] = nums[i]; 
+                } 
+                else {
+                        nums[i] = nums[i + 1];
+                }
+            }
+        }
+        return true;
+    }
+};
