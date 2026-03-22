35adb87ce9465fc1a06469d657ff4d1658a29c21
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-22 21:43:21 +0530

    Richest Customer Wealth

---
+++ Matrix/1672_Richest_Customer_Wealth.cpp
@@ -0,0 +1,21 @@
+// Time Complexity - O(n x m)
+// Space Complexity - O(1)
+
+// Medium
+// Leecode - https://leetcode.com/problems/richest-customer-wealth
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int sum = 0;
+        int max_wealth = 0;
+        for(int i = 0;i <accounts.size();i++){
+            sum = 0;
+            for(int j = 0;j<accounts[i].size();j++){
+                sum += accounts[i][j]; 
+            }
+            max_wealth =  max(max_wealth,sum);
+        }
+        return max_wealth;
+    }
+};
