8951c9824fda8667f058ed3d60eddafaff394c72
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-22 19:56:47 +0530

    Count Negative Numbers in a Sorted Matrix

---
+++ Matrix/1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp
@@ -0,0 +1,28 @@
+// Time Complexity - O(m + n)
+// Space Complexity - O(1)
+
+// Medium
+// Leecode - https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int count = 0;
+        
+        int r = 0;
+        int c = cols-1;
+        
+        while (r < rows && c >= 0) {
+            if (grid[r][c] < 0) {
+                count += (rows - r);
+                c--;
+            }
+            else {
+                r++;
+            }
+        }
+        return count;
+    }
+};
