6d5b0371927c160defa64d00612ea388a0819c83
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-22 20:38:17 +0530

    Number of Students Doing Homework at a Given Time

---
+++ Arrays/1450_Number_of_students_Doing_Homework_at_a_Given_Time.cpp
@@ -0,0 +1,16 @@
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Easy
+// Leecode - https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time
+
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int count=0;
+        for(int i=0;i<startTime.size();i++){
+            if(queryTime>=startTime[i] && queryTime<=endTime[i]) count++;
+        }
+        return count;
+    }
+};
