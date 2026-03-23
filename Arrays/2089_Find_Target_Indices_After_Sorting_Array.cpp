78f88d80ef5f58cc80ef9b6374d63349140916b8
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-03-23 08:31:57 +0530

    Find Target Indices After Sorting Array

---
+++ Arrays/2089_Find_Target_Indices_After_Sorting_Array.cpp
@@ -0,0 +1,20 @@
+// Time Complexity - O(n log n)
+// Space Complexity - O(1)
+
+// Easy 
+// Leetcode - https://leetcode.com/problems/find-target-indices-after-sorting-array
+
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int> result;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i <nums.size(); i++){
+            if(nums[i] == target){
+                result.push_back(i);
+            }
+        }
+        return result;
+        
+    }
+};
