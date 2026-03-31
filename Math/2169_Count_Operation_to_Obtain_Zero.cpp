5926562271b979c46fc5425ad22a220b4e258294
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-01 00:13:36 +0530

    Count Operation to Obtain Zero

---
+++ Math/2169_Count_Operation_to_Obtain_Zero.cpp
@@ -0,0 +1,24 @@
+// Time Complexity -O(n)
+// Space Complexity - O(1) 
+
+// Easy
+// Leetcode - https://leetcode.com/problems/count-operations-to-obtain-zero
+
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int count = 0;
+
+        while(num1 > 0 && num2 > 0){
+            if(num1 >= num2){
+                num1 -= num2;
+                count++;
+            }
+            else {
+                num2 -= num1;
+                count++;
+                } 
+        }
+        return count ;
+    }
+};
