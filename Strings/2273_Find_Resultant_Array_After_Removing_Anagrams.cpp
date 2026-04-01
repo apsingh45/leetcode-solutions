d4842d39d1abae6de49a3fc277e59c7cc0f0a85f
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-01 07:54:49 +0530

    Find Resultant Array After Removing Anagrams

---
+++ Strings/2273_Find_Resultant_Array_After_Removing_Anagrams.cpp
@@ -0,0 +1,24 @@
+// Time Complexity - O(n^2)
+// Space Complexity - O(n)
+
+// Medium
+// Leetcode - https://leetcode.com/problems/find-resultant-array-after-removing-anagrams
+
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        int n = words.size();
+        for(int i = 0;i < n-1;i++){
+            string temp1 = words[i];
+            string temp2 = words[i+1];
+            sort(temp1.begin(),temp1.end());
+            sort(temp2.begin(),temp2.end());
+            if(temp1 == temp2){
+                words.erase(words.begin() + i+1);
+                i--;
+                n--;
+            }
+        }
+        return words;
+    }
+};
