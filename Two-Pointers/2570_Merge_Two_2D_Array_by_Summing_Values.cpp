875065c95045d0b017a75e5df6ef0ad841adc9be
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-01 21:46:43 +0530

    Merge Two 2D Array by Summing Values

---
+++ Two-Pointers/2570_Merge_Two_2D_Array_by_Summing_Values.cpp
@@ -0,0 +1,33 @@
+// Time Complexity - O(n + m)
+// Space Complexity - O(n + m)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values
+
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        vector<vector<int>> result;
+        int i = 0;
+        int j = 0;
+        while (i<nums1.size() && j<nums2.size()){
+            if(nums1[i][0] == nums2[j][0]){
+                result.push_back({nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            } 
+            else if (nums1[i][0] < nums2[j][0]){
+                result.push_back(nums1[i]);
+                i++;
+            } 
+            else {
+                result.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i<nums1.size()) result.push_back(nums1[i++]);
+        while (j<nums2.size()) result.push_back(nums2[j++]);
+
+        return result;
+    }
+};
