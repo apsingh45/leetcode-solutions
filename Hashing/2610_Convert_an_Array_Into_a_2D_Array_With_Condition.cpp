10a895a053488ba1f9f45fb2a4b1b15398f9a3aa
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-02 07:35:51 +0530

    Convert an Array Into a 2D Array With Condition

---
+++ Hashing/2610_Convert_an_Array_Into_a_2D_Array_With_Condition.cpp
@@ -0,0 +1,29 @@
+// Time Complexity - O(n^2)
+// Space Complexity - O(n)
+
+// Medium
+// Leetcode Solutions - https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions
+
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<vector<int>>result;
+        map<int,int>freq;
+        for(auto it : nums){
+            freq[it]++;
+        }
+        for(int i = 0;i<nums.size();i++){
+            vector<int>temp;
+            for(auto &a : freq){
+                if(a.second != 0){
+                    temp.push_back(a.first);
+                    a.second--;
+                    i++;
+                }
+            }
+            result.push_back(temp);
+            i--;
+        }
+        return result;
+    }
+};
