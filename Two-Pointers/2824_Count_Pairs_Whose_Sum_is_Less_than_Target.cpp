71b9f3472a827ad2a51358d2df2c9888b9b42f74
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-02 18:24:10 +0530

    Count Pairs Whose Sum is Less Than Target

---
+++ Two-Pointers/2824_Count_Pairs_Whose_Sum_is_Less_than_Target.cpp
@@ -0,0 +1,18 @@
+// Time Complexity - O(n^2)
+// Space Complexity - O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) { 
+                if(nums[i] + nums[j] < target) count++;
+            }
+        }
+        return count;
+    }
+};
