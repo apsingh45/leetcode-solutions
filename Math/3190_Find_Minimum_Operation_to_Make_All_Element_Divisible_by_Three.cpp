b98bbf846bed84bbb12f5f3b4c0731778be65bb5
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-02 22:18:49 +0530

    Find Minimum Operation to Make All Element Divisible by Three

---
+++ Math/3190_Find_Minimum_Operation_to_Make_All_Element_Divisible_by_Three.cpp
@@ -0,0 +1,18 @@
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int count = 0;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]%3 == 0){
+                count++;
+            }
+        }
+        return nums.size()-count;
+    }
+};
