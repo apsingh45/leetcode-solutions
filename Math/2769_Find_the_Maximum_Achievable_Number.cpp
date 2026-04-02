4bf55aae5b53620d21b7fbcb3984f35fc6556690
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-02 07:52:50 +0530

    Find the Maxiimum Achievable Number

---
+++ Math/2769_Find_the_Maximum_Achievable_Number.cpp
@@ -0,0 +1,12 @@
+// Time Complexity - O(1)
+// Space Complexity - O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/find-the-maximum-achievable-number
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num+(2*t);
+    }
+};
