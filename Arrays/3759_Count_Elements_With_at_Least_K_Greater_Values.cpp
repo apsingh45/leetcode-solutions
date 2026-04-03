3c2a624a7f77f20ee09f62d331dca734c3fa2ca1
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-03 22:33:32 +0530

    Count Element With at Least K Greater Values.

---
+++ Arrays/3759_Count_Elements_With_at_Least_K_Greater_Values.cpp
@@ -0,0 +1,24 @@
+// Time Complexity - O(n log n)
+// Space Complexity - O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/count-elements-with-at-least-k-greater-values
+
+class Solution {
+public:
+    int countElements(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int result = 0;
+        int n = nums.size();
+        for(int i = 0;i<n;){
+            int val = nums[i];
+            int idx = upper_bound(nums.begin(), nums.end(), val) - nums.begin();
+            int gc = n - idx;
+            int freq = idx - i;
+            if(gc >= k)result+=freq;
+
+            i = idx;
+        }
+        return result;
+    }
+};
