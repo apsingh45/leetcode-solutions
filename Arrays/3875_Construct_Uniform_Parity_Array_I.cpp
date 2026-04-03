8117f27dbd5ea95b45f4ca9531c64e630dff5555
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-03 23:03:22 +0530

    Construct Uniform Parity Array

---
+++ Arrays/3875_Construct_Uniform_Parity_Array_I.cpp
@@ -0,0 +1,12 @@
+// Time Complexity - O(1)
+// Space Complexity - O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/construct-uniform-parity-array-i
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
