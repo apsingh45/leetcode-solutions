790b7b6349ab925168640b7d7ce3bd00323b24d0
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-08 23:12:35 +0530

    Longest Common Prefix

---
+++ Strings/14_Longest_Common_Prefix.cpp
@@ -0,0 +1,25 @@
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+// Easy
+// Leetcode - https://leetcode.com/problems/longest-common-prefix
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+
+        int i = 0;
+        while(i<strs[0].size()){
+            char ch = strs[0][i];
+            for(int j = 1;j < strs.size();j++){
+                if(i >= strs[j].size() || strs[j][i] != ch){
+                    return ans;
+                }
+            }
+            ans += ch;
+            i++;
+        }
+        return ans;
+    }
+};
