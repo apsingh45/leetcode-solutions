bccfdcf203183d0ebd8194863937c1553c22cfac
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-04-13 09:04:39 +0530

    Minimum Distance to the Target Element

---
+++ Arrays/1848_Minimum_Dustance_to_the_Target_Element.cpp
@@ -0,0 +1,18 @@
+// Time Complexity - O(n)
+// SPace Complexity - O(n)
+
+// Easy 
+// Leetcode - https://leetcode.com/problems/minimum-distance-to-the-target-element
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans = INT_MAX;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == target){
+                ans = min(ans,abs(i-start));
+            }
+        }
+        return ans;
+    }
+};
