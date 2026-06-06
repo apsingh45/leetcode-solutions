b2c271fb1a6e9ff46f6599a3d45fcaa635c5a1ad
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-06-06 21:47:43 +0530

    Subtree of Another Tree

---
+++ Tree/572_Subtree_of_Another_Tree.cpp
@@ -0,0 +1,55 @@
+// Time Complexity - O(N*M)
+// Space Complexity - O(H) ,height of tree
+
+// Easy 
+// Leetcode - https://leetcode.com/problems/subtree-of-another-tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isIdentical(TreeNode* root1, TreeNode* root2){
+        if(root1 == NULL && root2 == NULL){
+            return true;
+        } else if(root1 == NULL || root2 == NULL){
+            return false;
+        }
+
+        if(root1->val != root2->val){
+            return false;
+        }
+
+        return isIdentical(root1->left, root2->left) && 
+                isIdentical(root1->right, root2->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root == NULL && subRoot == NULL){
+            return true;
+        } else if(root == NULL || subRoot == NULL){
+            return false;
+        }
+
+        if(root->val == subRoot->val){
+            if(isIdentical(root,subRoot)){
+                return true;
+            }
+        }
+
+        bool isLeftSubtree = isSubtree(root->left,subRoot);
+
+        if(!isLeftSubtree){
+            return isSubtree(root->right,subRoot);
+        }
+
+        return true;
+    }
+};
