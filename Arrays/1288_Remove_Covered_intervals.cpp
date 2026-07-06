e0474498e6f5dfe3a9c7674b989a8ff2b8c31c36
Anant Pratap Singh <anantpratapsingh2k24@gmail.com>
2026-07-06 06:48:27 +0530

    Remove Covered Intervals

---
+++ Arrays/1288_Remove_Covered_intervals.cpp
@@ -0,0 +1,26 @@
+// Time Complexity - O(N log N)
+// Space Complexity - O(1)
+
+// Medium
+// Leetcode - https://leetcode.com/problems/remove-covered-intervals
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(),[](vector<int>& a, vector<int>& b){
+                if(a[0] == b[0]) return a[1] > b[1];
+                return a[0] < b[0];
+                });
+
+        int count = 0;
+        int maxEnd = 0;
+        for(auto &interval : intervals){
+            if(interval[1] > maxEnd) {
+                count++;
+                maxEnd = interval[1];
+            }
+        }
+
+        return count;
+    }
+};
